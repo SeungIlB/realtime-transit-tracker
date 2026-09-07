f5e5eb67694b6d910ba27e50dfb1ff3e30d7060e
SeungIl <seoung1515@naver.com>
2026-09-07 16:12:43 +0900

    fix(frontend): 웹 환경 TDS 초기화 오류 수정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { TDSMobileProvider } from '@toss/tds-mobile'
+import { ThemeProvider } from '@toss/tds-mobile'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
@@ -17,20 +17,12 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TDSMobileProvider
-      userAgent={{
-        fontA11y: undefined,
-        fontScale: undefined,
-        isAndroid: /Android/i.test(navigator.userAgent),
-        isIOS: /iPhone|iPad|iPod/i.test(navigator.userAgent),
-        colorPreference: 'light',
-      }}
-    >
+    <ThemeProvider>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </QueryClientProvider>
-    </TDSMobileProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
